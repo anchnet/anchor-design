28a32c9e52d52c66af4c18a3c36673755594caad
刘志猛 <liuzhimeng@LymanDeMacBook-Pro.local>
2017-06-27 11:14:39 +0800

    优化 vQuery.js

--- src/mixin/vQuery.js
+++ src/mixin/vQuery.js
@@ -6,18 +6,21 @@ class VQuery {
   init (element) {
     if (!element) return
     let $selector = this.selector
-    if (typeof element === 'String') {
-      let type = element.slice(0, 1)
-      let node = element.slice(1)
-      if (type === '#') {
-        $selector = document.getElementById(node)
-      } else if (type === '.') {
-        $selector = document.getElementsByClassName(node)
-      } else {
-        $selector = document.getElementsByName(node)
+
+    if (!element) $selector = this
+
+    if (element.includes(' ')) {
+      let selectorArray = element.toString().split(' ')
+      $selector = document
+      for (let selector of selectorArray.values()) {
+        $selector = $selector.querySelectorAll(selector)
       }
     } else {
-      $selector = element
+      if (element.startsWith('#')) {
+        $selector = document.querySelector(element)
+      } else {
+        $selector = document.querySelectorAll(element)
+      }
     }
 
     this.selector = $selector
@@ -26,12 +29,47 @@ class VQuery {
   }
 
   on (type, handler, params) {
-    this.selector.addEventListener(type, handler.bind(this), params)
+    if (this.selector.addEventListener) {
+      this.on = () => {
+        this.selector.addEventListener(type, handler.bind(this), params)
+        return this
+      }
+    } else if (this.selector.attachEvent) {
+      this.on = () => {
+        this.selector.attachEvent('on' + type, handler.bind(this))
+        return this
+      }
+    } else {
+      this.on = () => {
+        this.selector['on' + type] = handler.bind(this)
+        return this
+      }
+    }
+    return this
+  }
+
+  off (type, handler, useCapture) {
+    if (this.selector.removeEventListener) {
+      this.on = () => {
+        this.selector.removeEventListener(type, handler.bind(this), useCapture)
+        return this
+      }
+    } else if (this.selector.detachEvent) {
+      this.on = () => {
+        this.selector.detachEvent('on' + type, handler.bind(this))
+        return this
+      }
+    } else {
+      this.on = () => {
+        this.selector['on' + type] = null
+        return this
+      }
+    }
     return this
   }
 
   mouseenter (handler, params) {
-    this.on('mouseenter', handler)
+    this.on('mouseenter', handler, params)
     return this
   }
 
